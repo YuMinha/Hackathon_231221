d16fb063b0738c1c3c410fe3d994395abf4d32d0
유민하 <ul8822@naver.com>
2023-12-18 02:10:01 +0900

    [로봇] 호출 위치로 가는 기능
    E키 누르면 해당 위치로 이동함
    로봇 navagent 콜라이더 위치 이상해서 모델 만들때 제대로 만들면 괜찮을듯

---
+++ Assets/RobotController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RobotController : MonoBehaviour
+{
+    public Transform player;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        // NavMeshAgent 컴포넌트 가져오기
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        // 만약 플레이어가 설정되지 않았다면, 예외처리
+        if (player == null)
+        {
+            Debug.LogError("플레이어가 설정되지 않았습니다!");
+        }
+    }
+
+    void Update()
+    {
+        // E 키를 눌렀을 때 로봇이 플레이어를 향해 이동
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            MoveToPlayer();
+        }
+    }
+
+    void MoveToPlayer()
+    {
+        // NavMeshAgent를 사용하여 플레이어의 위치로 이동
+        navMeshAgent.SetDestination(player.position);
+    }
+}
